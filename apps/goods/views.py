deddd4c9912feac8b2f5e0907497f771c7c3dd49
chenzwcc <2552770507@qq.com>
2019-07-19 16:22:01 +0800

    完成Djangorestframework的searchfilter功能

--- apps/goods/views.py
+++ apps/goods/views.py
@@ -1,5 +1,3 @@
-import django_filters
-from django.shortcuts import render
 
 # Create your views here.
 from rest_framework.response import Response
@@ -9,31 +7,14 @@ from .models import Goods
 from .serializers import GoodsSerializer
 from .custom_pagin import GoodsPagination
 
-#class GoodsListView(APIView):
-#    def get(self,request,format=None):
-#        goods = Goods.objects.all()
-#        goods_serializer = GoodsSerializer(goods,many=True)
-#        return Response(goods_serializer.data)
-
-from rest_framework.generics import ListAPIView
-
-
-#class GoodsListView(ListAPIView):
-#    """
-#    商品列表
-#    """
-#    pagination_class = GoodsPagination  # 分页
-#    queryset = Goods.objects.all()
-#    serializer_class = GoodsSerializer
-
 from rest_framework.mixins import ListModelMixin
 from rest_framework.viewsets import GenericViewSet
-from goods.custom_filter import GoodsFilter
+
+from rest_framework import filters
 
 class GoodsListViewSet(ListModelMixin,GenericViewSet):
     queryset = Goods.objects.all()
     pagination_class = GoodsPagination  # 分页
-    filter_backends = [django_filters.rest_framework.DjangoFilterBackend]
-    # filter_fields = ('name',)
-    filter_class = GoodsFilter
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['name']
     serializer_class = GoodsSerializer
